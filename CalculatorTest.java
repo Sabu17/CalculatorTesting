f49a402741ea75becd71b3f589204497e76b5129
Sabu17 <153213403+Sabu17@users.noreply.github.com>
2023-12-09 17:53:52 +0300

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,27 +1,27 @@
-import static org.junit.jupiter.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
   
   @Test
   public void addTest() {
-    assertEquals(5, Calculator.sum(2, 3), 0);
+    org.junit.Assert.assertTrue(5, Calculator.sum(2, 3), 0);
   }
  
 
 @Test
   public void subtractTest() {
-    assertEquals(2, Calculator.sub(5, 3), 0);
+    org.junit.Assert.assertTrue(2, Calculator.sub(5, 3), 0);
   }
 
   @Test
   public void multiplyTest() {
-    assertEquals(12, Calculator.mult(3, 4), 0);
+    org.junit.Assert.assertTrue(12, Calculator.mult(3, 4), 0);
   }
 
   @Test
   public void divideTest() {
-    assertEquals(5, Calculator.div(10, 2), 0);
+    org.junit.Assert.assertTrue(5, Calculator.div(10, 2), 0);
   }
 }
 
