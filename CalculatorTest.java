8554541e2be016c65d7b042355f22af3b2493e1b
Sabu17 <153213403+Sabu17@users.noreply.github.com>
2023-12-09 18:12:56 +0300

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -5,23 +5,23 @@ public class CalculatorTest {
   
   @Test
   public void addTest() {
-    org.junit.Assert.assertTrue(5, Calculator.sum(2, 3), 0);
+    assertTrue(5, Calculator.sum(2, 3), 0);
   }
  
 
 @Test
   public void subtractTest() {
-    org.junit.Assert.assertTrue(2, Calculator.sub(5, 3), 0);
+    assertTrue(2, Calculator.sub(5, 3), 0);
   }
 
   @Test
   public void multiplyTest() {
-    org.junit.Assert.assertTrue(12, Calculator.mult(3, 4), 0);
+    assertTrue(12, Calculator.mult(3, 4), 0);
   }
 
   @Test
   public void divideTest() {
-    org.junit.Assert.assertTrue(5, Calculator.div(10, 2), 0);
+    assertTrue(5, Calculator.div(10, 2), 0);
   }
 }
 
